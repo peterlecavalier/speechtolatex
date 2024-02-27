25b3f3b3877084df5431e51dfed20a740b9f9360
loganbarnhart01 <logan.barnhart.01@gmail.com>
2024-02-27 00:03:29 -0700

    simplified loading test

--- modelInfo/loading_test.py
+++ modelInfo/loading_test.py
@@ -1,27 +1,3 @@
-# import transformers
-# import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
-# import bitsandbytes
-# import accelerate
-# 
-# from peft import (
-#   LoraConfig,
-#   PeftConfig,
-#   PeftModel,
-#   get_peft_model,
-#   prepare_model_for_kbit_training,
-# )
-# 
-# from transformers import (
-#   AutoTokenizer,
-#   AutoConfig,
-#   AutoModelForCausalLM,
-#   AutoModelForSequenceClassification,
-#   BitsAndBytesConfig,
-#   Trainer,
-# )
-
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
@@ -37,37 +13,3 @@ def main():
   print(f"Input: {input}\n")
   print("-"*50 + "Test Output" + "-"*50)
   print(tokenizer.decode(output[0], skip_special_tokens = True))
-
-
- # MODEL_NAME = "meta-llama/Llama-2-7b-hf"
- # tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
- # tokenizer.pad_token = tokenizer.eos_token
-
- # model = AutoModelForCausalLM.from_pretrained(
- #   MODEL_NAME,
- #   device_map = 'auto',
- #   trust_remote_code = True,
- #   quantization_config = bnb_config
- # )
-
- # model = get_peft_model(model, lora_config)
- # print_trainable_parameters(model)
-
-# bnb_config = BitsAndBytesConfig(
-#   load_in_4bit = True,
-#   bnb_4bit_use_double_quant = True,
-#   bnb_4bit_quant_type = 'nf4',
-#   bnb_4bit_compute_dtype = torch.bfloat16
-# )
-# 
-# lora_config = LoraConfig(
-#   r = 16,
-#   lora_alpha = 32,
-#   target_modules = ["q_proj", "v_proj"],
-#   lora_dropout = 0.05,
-#   bias = 'none',
-#   task_type = 'causal_LM'
-# )
-
-if __name__ == "__main__":
-  main()
