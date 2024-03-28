f0dd3af13245a3bbbc21a6756d55530fc3c3c2cb
Paul Rodriguez <mildmoose@pm.me>
2024-03-27 18:35:24 -0700

    Fixes

--- api/app.py
+++ api/app.py
@@ -6,12 +6,14 @@ from firebase_admin import credentials, auth, storage
 from dotenv import load_dotenv
 import os
 from convert import compile_latex, upload_pdf
+from flask_cors import CORS
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Initialize Flask app
 app = Flask(__name__)
+# CORS(app)
 
 # Initialize Firebase Admin SDK
 FIREBASE_PROJECT_ID = os.getenv('FIREBASE_PROJECT_ID')
@@ -74,6 +76,57 @@ def get_file(file_id):
                     return jsonify({'error': 'File not found or access denied.'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+
+@app.route('/files/<int:file_id>', methods=['PATCH'])
+def patch_file(file_id):
+    # Verify Firebase auth token
+    id_token = request.headers.get('Authorization').split('Bearer ')[1]
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        user_id = decoded_token['uid']
+    except Exception as e:
+        return jsonify({'error': 'Invalid or expired Firebase token.'}), 401
+
+
+    # Extract file data from request
+    try:
+        data = request.json
+        title = data.get('title')
+        latex = data.get('latex')
+        text = data.get('text')
+
+        
+        if not title:  # title is required
+            return jsonify({'error': 'Title is required.'}), 400
+    except Exception as e:
+        return jsonify({'error': 'Invalid input.'}), 400
+
+
+
+    # Retrieve the specific file from MySQL database
+    try:
+
+
+        with pymysql.connect(**db_config) as connection:
+            with connection.cursor() as cursor:
+                sql = "UPDATE files SET title = %s, latex = %s, text = %s WHERE id = %s AND userId = %s"
+                cursor.execute(sql, (title, latex, text, file_id, user_id))
+
+                if cursor.rowcount > 0:
+                    connection.commit()
+
+                    # If rows were updated, you can return a success response
+                    # Note: You may need to adjust this part according to your application's requirements
+                    return jsonify({'message': 'File updated successfully.'}), 200
+                else:
+                    return jsonify({
+                        'error': 'File not found or access denied.',
+                        'user_id': user_id,
+                        'file_id': file_id
+                    }), 403
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 # Delete a file, user must own the file
@@ -86,7 +139,7 @@ def delete_file(file_id):
         user_id = decoded_token['uid']
     except Exception as e:
         return jsonify({'error': 'Invalid or expired Firebase token.'}), 401
-
+    
     # Delete the specific file from MySQL database
     try:
         with pymysql.connect(**db_config) as connection:
@@ -171,7 +224,6 @@ def upload_pdf_logic():
     except Exception as e:
         return jsonify({'error': 'Invalid input.'}), 400
 
-    # Insert file into MySQL database
     try:
         compile_latex(id, latex)
         upload_pdf(id, user_id)
