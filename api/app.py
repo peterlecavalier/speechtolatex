57ab150ff4f80ea1fbe89ef061f46595a32b2dcd
Paul Rodriguez <mildmoose@pm.me>
2024-04-11 17:55:12 -0600

    Added ability to change DB port

--- api/app.py
+++ api/app.py
@@ -25,6 +25,7 @@ firebase_admin.initialize_app(cred, {'storageBucket': f'{FIREBASE_PROJECT_ID}.ap
 db_config = {
     'host': os.getenv('DB_HOST'),
     'user': os.getenv('DB_USER'),
+    'port': int(os.getenv('DB_PORT')),
     'password': os.getenv('DB_PASSWORD'),
     'database': os.getenv('DB_DATABASE'),
     'cursorclass': pymysql.cursors.DictCursor
